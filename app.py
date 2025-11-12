f48a322ca055a0b75275dd735726f8fe23076781
Phosnay <nils.romano@gmail.com>
2025-11-12 12:17:35 +0100

    (1) : Stable
    Created : videos.html, index.html
    Modified : app.py
    
    Integrated videos don't work but show up.

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
+
+
+@app.route('/videos', methods=['GET'])
+def videos():
+    urls = ['https://www.youtube.com/embed/watch?v=Rof660OEA3E', 
+            'https://www.youtube.com/embed/watch?v=yakOBIoyoik']
+    return render_template('videos.html', urls=urls)
+
+#     name = request.args.get("name", "Flask")
+#     return f"Hello, {escape(name)}!"
+#     return "<p>Videos Page</p>"
