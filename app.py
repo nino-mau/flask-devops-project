fe10ea7335f837b00644fe15b967c78a73b7a37a
Phosnay <nils.romano@gmail.com>
2025-11-12 14:09:40 +0100

    (2) : Stable
    Added conc for ytb

--- app.py
+++ app.py
@@ -10,10 +10,21 @@ def hello_world():
 
 @app.route('/videos', methods=['GET'])
 def videos():
-    urls = ['https://www.youtube.com/embed/watch?v=Rof660OEA3E', 
-            'https://www.youtube.com/embed/watch?v=yakOBIoyoik']
+    ytb_prefix = 'https://www.youtube.com/embed/watch?v='
+    urls = ['Rof660OEA3E', 'yakOBIoyoik']
+
+    urls = [ytb_prefix + url for url in urls]
+    print(urls)
     return render_template('videos.html', urls=urls)
 
 #     name = request.args.get("name", "Flask")
 #     return f"Hello, {escape(name)}!"
 #     return "<p>Videos Page</p>"
+
+# Source - https://stackoverflow.com/a
+# Posted by codegeek, modified by community. See post 'Timeline' for change history
+# Retrieved 2025-11-12, License - CC BY-SA 4.0
+
+app.run(debug=True)
+if __name__ == "__main__":
+    app.run
