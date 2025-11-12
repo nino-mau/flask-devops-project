f9d7e7c3a17e5d646f02f5b0ef2bac2cfc1b61eb
nino <nino.mauuary@gmail.com>
2025-11-12 12:04:18 +0100

    :sparkles:feat(data): implement initializing videos data from json

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask
 from flask import render_template
 from dotenv import load_dotenv
@@ -8,6 +9,11 @@ load_dotenv()
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.getenv('SECRET_KEY') 
+
+# Load videos data from json
+with open("./videos.json", "r") as f:
+    videos_data = json.load(f)
+
 @app.route('/')
-def home(name=None):
+def home():
     return render_template('home.html')
